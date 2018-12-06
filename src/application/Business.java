1259923e431394f806f47277e3d5e4973ebbb35c
LOOCEA <33818803+LOOCEA@users.noreply.github.com>
2018-12-06 11:43:19 +0800

    update

---
+++ src/application/Business.java
@@ -0,0 +1,63 @@
+package application;
+
+public class Business {
+	private int id;
+	private String name;
+	private String username;
+	private String password;
+	private String phone;
+	private String email;
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
--- src/application/Db.java
+++ src/application/Db.java
@@ -10,6 +10,18 @@ import java.util.ResourceBundle;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ArrayListHandler;
 
+/*
+ * ①ArrayHandler：     将查询结果的第一行数据，保存到Object数组中
+      ②ArrayListHandler     将查询的结果，每一行先封装到Object数组中，然后将数据存入List集合
+      ③BeanHandler     将查询结果的第一行数据，封装到user对象
+     ④BeanListHandler     将查询结果的每一行封装到user对象，然后再存入List集合
+     ⑤ColumnListHandler     将查询结果的指定列的数据封装到List集合中
+     ⑥MapHandler     将查询结果的第一行数据封装到map结合（key==列名，value==列值）
+     ⑦MapListHandler     将查询结果的每一行封装到map集合（key==列名，value==列值），再将map集合存入List集合
+     ⑧BeanMapHandler     将查询结果的每一行数据，封装到User对象，再存入mao集合中（key==列名，value==列值）
+     ⑨KeyedHandler     将查询的结果的每一行数据，封装到map1（key==列名，value==列值 ），然后将map1集合（有多个）存入map2集合（只有一个）
+     ⑩ScalarHandler     封装类似count、avg、max、min、sum......函数的执行结果
+ * */
 public class Db {
 
 	// 数据库连接地址
@@ -63,7 +75,6 @@ public class Db {
 	}
 
 	public static void main(String[] args) throws SQLException {
-
 		String sql = "select * from customer";
 		QueryRunner qr = new QueryRunner();
 
