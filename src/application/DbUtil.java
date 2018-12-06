eeb181cf54c96c1197638fa911afebae83a2102b
LOOCEA <33818803+LOOCEA@users.noreply.github.com>
2018-12-06 09:23:04 +0800

    import DbUtil

---
+++ src/application/DbUtil.java
@@ -0,0 +1,67 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class DbUtil {
+
+	// 数据库连接地址
+	public static String URL = "jdbc:mysql://127.0.0.1:3306/ordersystemby(yyandlzh)?useSSL=false&serverTimezone=GMT";
+	// 用户名
+	public static String USERNAME = "root";
+	// 密码
+	public static String PASSWORD = "123456";
+	// mysql的驱动类
+	public static String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+	private static ResourceBundle rb = ResourceBundle.getBundle("com.util.db.db-config");
+
+	// 使用静态块加载驱动程序
+	static {
+		URL = rb.getString("jdbc.url");
+		USERNAME = rb.getString("jdbc.username");
+		PASSWORD = rb.getString("jdbc.password");
+		DRIVER = rb.getString("jdbc.driver");
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 定义一个获取数据库连接的方法
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("获取连接失败");
+		}
+		return conn;
+	}
+
+	// 关闭数据库连接
+	public static void close(ResultSet rs, Statement stat, Connection conn) {
+		try {
+			if (rs != null)
+				rs.close();
+			if (stat != null)
+				stat.close();
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		DbUtil db = new DbUtil();
+
+	}
+
+}
