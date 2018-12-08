1cabc4896ac17786c324c05767facbc98eaf4706
ClothoAO <dell@DESKTOP-1K0J4J0>
2018-12-08 14:48:48 +0800

    登录界面的布局又移动了1、、&&补充了两个注册界面的fxid

--- src/application/controllerClass/RegisterAsBusinessController.java
+++ src/application/controllerClass/RegisterAsBusinessController.java
@@ -2,9 +2,32 @@ package application.controllerClass;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class RegisterAsBusinessController {
 	@FXML
+	private TextField txUserName;
+	@FXML
+	private Label lbUserName;
+	@FXML
+	private Label lbPsw;
+	@FXML
 	private Button btnReg;
+	@FXML
+	private Label lbPhoneNumber;
+	@FXML
+	private Label lbEmail;
+	@FXML
+	private TextField txPhoneNumber;
+	@FXML
+	private TextField txEmail;
+	@FXML
+	private PasswordField txPswConfirm;
+	@FXML
+	private PasswordField txPsw;
+	@FXML
+	private Label lbConfirm;
 
 }
