085d20e2fd74a942017118527a5979e442f9092c
ClothoAO <dell@DESKTOP-1K0J4J0>
2018-12-12 14:18:33 +0800

    删除了两个无用的frame，增加了商户注册，修改了用户主页的search按钮

--- src/application/frameClass/BusinessRegisterFrame.java
+++ src/application/frameClass/BusinessRegisterFrame.java
@@ -10,7 +10,7 @@ public class BusinessRegisterFrame {
 		try {
 			Parent root = FXMLLoader
 					.load(getClass().getClassLoader().getResource("application//fxml//BusinessRegister.fxml"));
-			Scene scene = new Scene(root, 533, 601);
+			Scene scene = new Scene(root, 550, 769);
 			// scene.getStylesheets().add(getClass().getResource("//application.css").toExternalForm());
 			scene.getStylesheets()
 					.add(getClass().getClassLoader().getResource("application//application.css").toExternalForm());
