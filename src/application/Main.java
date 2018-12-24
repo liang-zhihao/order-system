394c6070568a143a14fa1774878dfa3c1a621bc2
LOOCEA <33818803+LOOCEA@users.noreply.github.com>
2018-12-24 11:48:31 +0800

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -23,9 +24,7 @@ public class Main extends Application {
 
 			primaryStage.getIcons().add(new Image("application/fxml/img/233.jpg"));
 
-
 			primaryStage.setTitle("��¼����");
-
 
 			primaryStage.show();
 		} catch (Exception e) {
